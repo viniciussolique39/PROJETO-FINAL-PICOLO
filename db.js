533df75ee70b956a86c7981412be53773c3045f0
Vinicius Solique <vinicius.solique@estudante.ifms.edu.br>
2023-05-29 13:55:43 -0400

    Initial commit

---
+++ db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+let connect = async function () {
+  try {
+    if (global.connection) {
+      return Promise.resolve(global.connection);
+    }
+
+    const pool = new Pool({
+      connectionString: 'postgres://jezmpuvm:LTTlu-MlqNNv8Bv1cCaHYNcUECnMwLfQ@silly.db.elephantsql.com/jezmpuvm'
+    });
+
+    global.connection = pool;
+    return Promise.resolve(pool);
+  } catch (error) {
+    console.error('Erro ao estabelecer a conexão:', error);
+    throw error;
+  }
+};
+
+module.exports = { connect };
